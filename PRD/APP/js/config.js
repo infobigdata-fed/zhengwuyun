28061cd8b59c7876d7f27b2d08af4b35f1a7b5d9
jackLei <leiyong@infobigdata@com>
2017-05-17 18:28:50 +0800

    [PRD] Add:PRD

---
+++ PRD/APP/js/config.js
@@ -0,0 +1,1 @@
+window.config = {'showControlPanel': true, 'iOSGuide': true, 'showQRCode': false, 'shell': 'iPhone', 'background': '#C4C4C4', 'alwaysShowLinks': false, 'hoverShowLinks': false, 'hoverShowRemarks': true, 'showComments': true, 'hintLinks': true, 'scrollRemarks': true};
